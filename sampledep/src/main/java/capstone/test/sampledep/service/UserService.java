b5e49a3be92c653474dd718392e47130eeca6926
Aditya Basak <aditya.basak.11@gmail.com>
2021-06-10 15:05:51 -0700

    Final Changes to the Backend

--- sampledep/src/main/java/capstone/test/sampledep/service/UserService.java
+++ sampledep/src/main/java/capstone/test/sampledep/service/UserService.java
@@ -2,8 +2,10 @@ package capstone.test.sampledep.service;
 
 import capstone.test.sampledep.data.User;
 import capstone.test.sampledep.repository.UserRepository;
+import capstone.test.sampledep.request.EditUserRequest;
 import capstone.test.sampledep.request.LoginRequest;
 import capstone.test.sampledep.request.RegisterUserRequest;
+import capstone.test.sampledep.response.UserProfileResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -11,15 +13,29 @@ import org.springframework.util.StringUtils;
 
 import java.util.Collections;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private RatingsService ratingsService;
 
     public User getUserById(Long id) {
         return userRepository.findById(id).get();
+    }
+
+    public UserProfileResponse getUserProfile(Long userId) throws Exception{
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if(!optionalUser.isPresent())
+            throw new Exception("No such user with id " +  userId);
+        UserProfileResponse userProfileResponse = new UserProfileResponse();
+        userProfileResponse.setAccountDetails(optionalUser.get());
+        ratingsService.setUserProfileResponse(userId, userProfileResponse);
+        return userProfileResponse;
     }
 
     public User registerUser(RegisterUserRequest registerUserRequest) throws Exception{
@@ -34,6 +50,19 @@ public class UserService {
         user.setPhone(registerUserRequest.getPhone());
         user.setName(registerUserRequest.getName());
         user.setPassword(registerUserRequest.getPassword());
+        user.setImage(registerUserRequest.getImage());
+        return userRepository.save(user);
+    }
+
+    public User editUser(Long userId, EditUserRequest request) throws Exception{
+        User user = userRepository.findById(userId).get();
+        if (Objects.isNull(user)) {
+            throw new Exception("No such user with id : " + userId);
+        }
+        user.setPhone(request.getPhone());
+        user.setName(request.getName());
+        user.setPassword(request.getPassword());
+        user.setImage(request.getImage());
         return userRepository.save(user);
     }
 
